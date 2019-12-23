e272ccf812b12c0d297cc5a499977c0102e6d8c9
Ion Trifu <ion.trifu-EXT@continental-corporation.com>
2019-12-23 15:52:04 +0200

    oled display added + keys added for remote control

--- Keyes_L298P_RF_Robo_RF_Servo.ino
+++ Keyes_L298P_RF_Robo_RF_Servo.ino
@@ -10,6 +10,11 @@
    8 - UNUSED
  */
 //-----( Import needed libraries )-----
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -18,6 +23,34 @@ Servo myservo;
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define CE_PIN 8
 #define CSN_PIN 7
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
 //NOTE: the "LL" at the end of the constant is "LongLong" type
 const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe
 /*
@@ -37,7 +70,7 @@ int left_motor_direction=13;          // Motor B
 /*-----( Declare objects )-----*/
 RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 /*-----( Declare Variables )-----*/
-int joystick[6];  // 6 element array holding Joystick readings
+int joystick[8];  // 6 element array holding Joystick readings
 int speed = 0;
 int speedb = 0;
 int  xAxis, yAxis;
@@ -47,6 +80,7 @@ int buttonRight;
 int buttonDown;
 int buttonLeft;
 int servo_angle;
+int set_speed = 140;
 
 void setup()
 {
@@ -61,6 +95,36 @@ void setup()
   radio.setPALevel(RF24_PA_MIN);
   radio.openReadingPipe(1,pipe);
   radio.startListening();
+
+
+ // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+ // testdrawstyles();    // Draw 'stylized' characters
+  // Invert and restore display, pausing in-between
+  //display.invertDisplay(true);
+  // delay(1000);
+  display.invertDisplay(false);
+  delay(1000);
+  
 }
 
 void loop()
@@ -76,7 +140,20 @@ void loop()
       int buttonRight = joystick[3];
       int buttonDown  = joystick[4];
       int buttonLeft  = joystick[5];
-      
+
+      int E_BTN = joystick[6];
+      int F_BTN = joystick[7];
+
+      /*
+      if (E_BTN==0)
+      {
+      set_speed = set_speed+5;
+      }
+      else if (F_BTN==0)
+      {
+      set_speed = set_speed-5;
+      }
+      */
 // Y-axis used for forward and backward control  
  if (yAxis < 500 || buttonUp==0) {
     // Motors backward
@@ -88,7 +165,7 @@ void loop()
     {speed=0;}
 
     if (buttonUp==0)
-    {speedb=110;}
+    {speedb=set_speed-30;}
     else
     {speedb=0;}
 
@@ -99,6 +176,24 @@ void loop()
     analogWrite(right_motor_PWM, speed+speedb); 
     digitalWrite(left_motor_direction, LOW);
     analogWrite(left_motor_PWM, speed+speedb);
+
+
+      display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  
+  //  display.println(F("Speed"));
+  // display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+  // display.println(3.141592);
+
+  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("set Speed: ")); display.println(set_speed);
+  display.print(F("Speed: ")); display.println(speed+speedb);
+
+  display.display();
   }
 else if (yAxis > 510 || buttonDown==0) {
     // Motors forward
@@ -111,7 +206,7 @@ else if (yAxis > 510 || buttonDown==0) {
     {speed=0;}
         
     if (buttonDown==0)
-    {speedb=170;}
+    {speedb=set_speed+30;}
     else
     {speedb=0;}
 
@@ -122,6 +217,24 @@ else if (yAxis > 510 || buttonDown==0) {
     analogWrite(right_motor_PWM, speed+speedb); 
     digitalWrite(left_motor_direction, HIGH);
     analogWrite(left_motor_PWM, speed+speedb);
+
+  display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  
+  //  display.println(F("Speed"));
+  // display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+  // display.println(3.141592);
+
+  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("set Speed: ")); display.println(set_speed);
+  display.print(F("Speed: ")); display.println(speed+speedb);
+
+  display.display();
+    
   }
   // If joystick stays in middle the motors are not moving
   else {
@@ -133,6 +246,24 @@ else if (yAxis > 510 || buttonDown==0) {
     analogWrite(right_motor_PWM, speed);
     digitalWrite(left_motor_direction, LOW);
     analogWrite(left_motor_PWM, speed);
+
+
+  display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  
+  //  display.println(F("Speed"));
+  // display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+  // display.println(3.141592);
+
+  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("set Speed: ")); display.println(set_speed);
+  display.print(F("Speed: ")); display.println(speed);
+
+  display.display();
 
   }
 
@@ -161,4 +292,25 @@ else if (yAxis > 510 || buttonDown==0) {
       Serial.println(joystick[5]);
     */
       } 
+
+/*
+  void testdrawstyles(void) {
+  display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+  display.println(3.141592);
+
+  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("0x")); display.println(speedb, HEX);
+
+  display.display();
+  delay(2000);
+}
+*/
   
